48dce8237b433ee2a62f079ba3ddf2eb66406e2f
Kunal Verma <scokunal9@gmail.com>
2021-03-08 18:56:28 -0500

    Health and Menu
    Healthbar

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -16,6 +16,7 @@ public class PlayerBehaviour : MonoBehaviour
 
     public Vector3 velocity;
     public bool isGrounded;
+    public float Health = 100f;
 
     // Start is called before the first frame update
     void Start()
